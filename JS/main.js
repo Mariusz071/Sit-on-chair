56344b18eb5847d7cb8dbd7bd5492e9077687a31
Mariusz <siewruk.mariusz92@gmail.com>
2018-09-29 16:49:49 +0200

    Dodano funkcjonalne drop-down listy w formularzu

--- JS/main.js
+++ JS/main.js
@@ -20,20 +20,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //SLIDER
     const prevButton = document.querySelector('.arrow-left');
-    console.log(prevButton);
     const nextButton = document.querySelector('.arrow-right');
-    console.log(nextButton);
     const sliderElements = document.querySelectorAll('.slider > li');
-    console.log(sliderElements);
     let imgIndex = 0;
 
     sliderElements[imgIndex].classList.add('visible');
     const visibleElement = document.querySelector('li.visible');
     visibleElement.style.opacity = "1";
-
-
-    console.log(visibleElement);
-
 
     nextButton.addEventListener('click', function () {
         sliderElements[imgIndex].classList.remove('visible');
@@ -55,32 +48,34 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         sliderElements[imgIndex].classList.add('visible');
     })
+
+    /* FORM FUNCTIONS*/
+
+    const formDropdown = document.querySelectorAll('.drop_down_list');
+    const firstDropdown = formDropdown[0];
+    const secondDropdown = formDropdown[1];
+    const thirdDropdown = formDropdown[2];
+    //const dropButton = document.querySelectorAll('.list_arrow');
+    //console.log(dropButton);
+
+    formDropdown.forEach( function (el) {
+        let dropButton = el.querySelector('.list_arrow');
+        dropButton.addEventListener('click', function () {
+            var dropList = dropButton.parentElement.querySelector('.list_panel');
+            dropList.style.display = "block";
+            const optionItem = dropList.querySelectorAll('li');
+            optionItem.forEach( function (x) {
+                x.addEventListener('click', function () {
+                    const choosenItem = dropList.parentElement.firstElementChild;
+                    choosenItem.innerText = x.innerHTML;
+                    choosenItem.style.color = "#C2C2C2";
+                    if (choosenItem.innerText = x.innerHTML) {
+                        dropList.style.display = "none";
+                    }
+                })
+            })
+        })
+    })
 });
 
 
-
-//     li[imgIndex].classList.add('visible');
-//
-//
-//     nextButton.addEventListener('click', function () {
-//         li[imgIndex].classList.remove('visible');
-//         if ( imgIndex == 5) {
-//             imgIndex = 0;
-//         } else {
-//             imgIndex++;
-//         }
-//         li[imgIndex].classList.add('visible');
-//     });
-//
-//
-//     prevButton.addEventListener('click', function () {
-//         li[imgIndex].classList.remove('visible');
-//         if ( imgIndex == 0) {
-//             imgIndex = 5;
-//         } else {
-//             imgIndex--;
-//         }
-//         li[imgIndex].classList.add('visible');
-//     })
-//
-// })
