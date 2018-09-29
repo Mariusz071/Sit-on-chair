4435ede1410ca7f31d974bbb9d13bc21f7fbf527
Mariusz <siewruk.mariusz92@gmail.com>
2018-09-29 14:26:32 +0200

    Dodano slider oraz application section

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var li = document.querySelectorAll('.navbar > ul > li');
+
+    //DROPDOWN MENU
+    const li = document.querySelectorAll('.navbar > ul > li');
     li.forEach( function (el) {
          el.addEventListener('mouseover', function () {
              const dropdownMenu = el.querySelector('li > .dropdown-menu');
@@ -15,4 +17,70 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    //SLIDER
+    const prevButton = document.querySelector('.arrow-left');
+    console.log(prevButton);
+    const nextButton = document.querySelector('.arrow-right');
+    console.log(nextButton);
+    const sliderElements = document.querySelectorAll('.slider > li');
+    console.log(sliderElements);
+    let imgIndex = 0;
+
+    sliderElements[imgIndex].classList.add('visible');
+    const visibleElement = document.querySelector('li.visible');
+    visibleElement.style.opacity = "1";
+
+
+    console.log(visibleElement);
+
+
+    nextButton.addEventListener('click', function () {
+        sliderElements[imgIndex].classList.remove('visible');
+
+        if (imgIndex == 1) {
+            imgIndex = 0;
+        } else {
+            imgIndex++;
+        }
+        sliderElements[imgIndex].classList.add('visible');
+    })
+
+    prevButton.addEventListener('click', function () {
+        sliderElements[imgIndex].classList.remove('visible');
+        if (imgIndex == 0) {
+            imgIndex = 1;
+        } else {
+            imgIndex--;
+        }
+        sliderElements[imgIndex].classList.add('visible');
+    })
 });
+
+
+
+//     li[imgIndex].classList.add('visible');
+//
+//
+//     nextButton.addEventListener('click', function () {
+//         li[imgIndex].classList.remove('visible');
+//         if ( imgIndex == 5) {
+//             imgIndex = 0;
+//         } else {
+//             imgIndex++;
+//         }
+//         li[imgIndex].classList.add('visible');
+//     });
+//
+//
+//     prevButton.addEventListener('click', function () {
+//         li[imgIndex].classList.remove('visible');
+//         if ( imgIndex == 0) {
+//             imgIndex = 5;
+//         } else {
+//             imgIndex--;
+//         }
+//         li[imgIndex].classList.add('visible');
+//     })
+//
+// })
