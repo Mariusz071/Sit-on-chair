4916c9824cc80a56657b0cf8c3c33f29cea2c6d6
Mariusz <siewruk.mariusz92@gmail.com>
2018-10-02 01:50:43 +0200

    Re-made calculator funcionality

--- JS/main.js
+++ JS/main.js
@@ -2,18 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //DROPDOWN MENU
     const li = document.querySelectorAll('.navbar > ul > li');
-    li.forEach( function (el) {
-         el.addEventListener('mouseover', function () {
-             const dropdownMenu = el.querySelector('li > .dropdown-menu');
-             if(dropdownMenu !== null) {
-                 dropdownMenu.style.display = "block";
-             }
-         });
+    li.forEach(function (el) {
+        el.addEventListener('mouseover', function () {
+            const dropdownMenu = el.querySelector('li > .dropdown-menu');
+            dropdownMenu.style.transition = "all 1s ease 3s"
+            if (dropdownMenu !== null) {
+                dropdownMenu.style.display = "block";
+                dropdownMenu.style.opacity = "1";
+            }
+        });
 
         el.addEventListener('mouseout', function () {
             const dropdownMenu = el.querySelector('li >.dropdown-menu');
-            if(dropdownMenu !== null) {
+            if (dropdownMenu !== null) {
                 dropdownMenu.style.display = "none";
+                dropdownMenu.style.opacity = "0";
             }
         });
     });
@@ -37,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
             imgIndex++;
         }
         sliderElements[imgIndex].classList.add('visible');
-    })
+    });
 
     prevButton.addEventListener('click', function () {
         sliderElements[imgIndex].classList.remove('visible');
@@ -47,34 +50,92 @@ document.addEventListener('DOMContentLoaded', function () {
             imgIndex--;
         }
         sliderElements[imgIndex].classList.add('visible');
-    })
+    });
 
     /* FORM FUNCTIONS*/
-
     const formDropdown = document.querySelectorAll('.drop_down_list');
+
     const firstDropdown = formDropdown[0];
     const secondDropdown = formDropdown[1];
     const thirdDropdown = formDropdown[2];
-    //const dropButton = document.querySelectorAll('.list_arrow');
-    //console.log(dropButton);
 
-    formDropdown.forEach( function (el) {
-        let dropButton = el.querySelector('.list_arrow');
-        dropButton.addEventListener('click', function () {
-            var dropList = dropButton.parentElement.querySelector('.list_panel');
-            dropList.style.display = "block";
-            const optionItem = dropList.querySelectorAll('li');
-            optionItem.forEach( function (x) {
-                x.addEventListener('click', function () {
-                    const choosenItem = dropList.parentElement.firstElementChild;
-                    choosenItem.innerText = x.innerHTML;
-                    choosenItem.style.color = "#C2C2C2";
-                    if (choosenItem.innerText = x.innerHTML) {
-                        dropList.style.display = "none";
-                    }
-                })
-            })
+    const dropButtons = document.querySelectorAll('.list_arrow');
+    const dropdownList = document.querySelectorAll('.list_panel');
+    const calcTitle = document.querySelector('h4.title');
+    const chairPrice = document.querySelector('h4.value');
+    const chairColor = document.querySelector('.color');
+    const colorPrice = document.querySelector('span.color.value');
+    const chairMaterial = document.querySelector('.pattern');
+    const materialPrice = document.querySelector('span.pattern.value');
+
+    const transportCheckbox = document.querySelector('#checkbox-transport');
+    const transportPrice = document.querySelector('span.transport.value');
+
+    const totalSum = document.querySelector('.sum strong');
+    console.log(totalSum);
+
+
+
+
+    var firstItems = firstDropdown.querySelectorAll('li');
+    firstItems.forEach(function (el) {
+        dropButtons[0].addEventListener('click', function () {
+            dropdownList[0].style.display ="block";
+
+            el.addEventListener('click', function () {
+                const choosenItem = dropdownList[0].parentElement.firstElementChild;
+                choosenItem.innerText = el.innerHTML;
+                choosenItem.style.color = "#C2C2C2";
+                if (choosenItem.innerText = el.innerHTML) {
+                    dropdownList[0].style.display = "none";
+                }
+                calcTitle.innerText = choosenItem.innerText;
+                chairPrice.innerHTML = el.dataset.price;
+            });
         })
+    });
+
+    var secondItems = secondDropdown.querySelectorAll('li');
+    secondItems.forEach(function (el) {
+        dropButtons[1].addEventListener('click', function () {
+            dropdownList[1].style.display = "block";
+
+            el.addEventListener('click', function () {
+                const choosenItem = dropdownList[1].parentElement.firstElementChild;
+                choosenItem.innerText = el.innerHTML;
+                choosenItem.style.color = "#C2C2C2";
+                if (choosenItem.innerText = el.innerHTML) {
+                    dropdownList[1].style.display = "none";
+                }
+                chairColor.innerText = el.innerText;
+                colorPrice.innerHTML = "Gratis";
+            })
+        });
+
+        var thirdItems = thirdDropdown.querySelectorAll('li');
+        thirdItems.forEach(function (el) {
+            dropButtons[2].addEventListener('click', function () {
+                dropdownList[2].style.display = "block";
+
+                el.addEventListener('click', function () {
+                    const choosenItem = dropdownList[2].parentElement.firstElementChild;
+                    choosenItem.innerText = el.innerHTML;
+                    choosenItem.style.color = "#C2C2C2";
+                    if (choosenItem.innerText = el.innerHTML) {
+                        dropdownList[2].style.display = "none";
+                    }
+                    chairMaterial.innerText = choosenItem.innerText;
+                    materialPrice.innerText = el.dataset.priceMaterial;
+                })
+            });
+        });
+        transportCheckbox.addEventListener('change', function () {
+            if (transportCheckbox.checked) {
+                transportPrice.innerText = transportCheckbox.dataset.transportPrice;
+            } else {
+                transportPrice.innerText = 0;
+            }
+        });
     })
 });
 
